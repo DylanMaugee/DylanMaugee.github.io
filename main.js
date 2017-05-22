84f0f13c3a5945f3f2b779fd55643c773f89ef60
DylanMaugee <maugee.dylan@gmail.com>
2017-05-22 09:58:06 +0200

    rework

---
+++ main.js
@@ -0,0 +1,74 @@
+var audioInputSelect = document.querySelector('select#audioSource');
+        var videoSelect = document.querySelector('select#videoSource');
+        var myCam = document.querySelector('#webcam')
+        var test;
+        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+
+        // Get the devices infos to display on page
+        function getMedias() {
+            navigator.mediaDevices.enumerateDevices().then(function(devices) {
+                    devices.forEach(function(device) {
+                        var option = document.createElement('option');
+                        option.value = device.deviceId;
+                        if (device.kind === 'audioinput') {
+                            option.text = device.label ||
+                                'microphone ' + (audioInputSelect.length + 1);
+                            audioInputSelect.appendChild(option);
+                        } else if (device.kind === 'videoinput') {
+                            option.text = device.label || 'camera ' + (videoSelect.length + 1);
+                            videoSelect.appendChild(option);
+                        } else {
+                            console.log('Error getting media of user ', device);
+                        }
+                    });
+                })
+                .catch(function(err) {
+                    console.log(err.name + ": " + err.message);
+                });
+        }
+
+        function startCam() {
+            //Get user medias info
+            if (navigator.getUserMedia) {
+                navigator.getUserMedia({
+                    video: true,
+                    audio: true
+                }, handleVideo, videoError);
+            }
+        }
+
+        function changeSources() {
+            var audioSrc = audioInputSelect.value;
+            var videoSrc = videoSelect.value;
+            var constraints = {
+                audio: {
+                    deviceId: audioSrc ? {
+                        exact: audioSrc
+                    } : undefined
+                },
+                video: {
+                    deviceId: videoSrc ? {
+                        exact: videoSrc
+                    } : undefined
+                }
+            };
+            navigator.mediaDevices.getUserMedia(constraints, handleVideo, videoError);
+        }
+
+        // Create the video/audio stream
+        function handleVideo(stream) {
+            getMedias();
+            console.log(stream);
+            window.stream = stream;
+            myCam.srcObject = stream;
+        }
+
+        function videoError(e) {
+            console.log('Error with video')
+        }
+
+        audioOutputSelect.onchange = changeAudioDestination;
+        audioInputSelect.onchange = changeSources;
+        videoSelect.onchange = changeSources;
+
+        startCam();
